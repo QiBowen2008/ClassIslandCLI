00b11e196fb29fe6e06969f063786d18682f9889
Buger <buger7852009@163.com>
2026-07-02 10:27:19 +0800

    添加构建脚本

---
+++ build.bat
@@ -0,0 +1,4 @@
+cd ./ClassIslandCLI
+dotnet publish -r win-x64 -c Release
+dotnet publish -r win-x86 -c Release
+dotnet publish -r win-arm64 -c Release
---
+++ build.sh
@@ -0,0 +1,3 @@
+cd ./ClassIslandCLI
+dotnet publish -r linux-x64 -c Release
+dotnet publish -r linux-arm64 -c Release
